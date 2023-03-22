7f6bda24ce8ddd756a4773466ebc6f076cea25a3
david0080 <davidbugayov@ya.ru>
2023-03-22 20:24:54 +0300

    added kotlin

--- app/src/main/java/com/convector/david_000/convector_valute/MainActivity.kt
+++ app/src/main/java/com/convector/david_000/convector_valute/MainActivity.kt
@@ -1,104 +1,88 @@
-package com.convector.david_000.convector_valute;
+package com.convector.david_000.convector_valute
 
-import android.app.Activity;
-import android.os.Bundle;
-import android.view.View;
-import android.widget.ArrayAdapter;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.Spinner;
-import android.widget.Toast;
+import android.app.Activity
+import android.os.Bundle
+import android.view.View
+import android.widget.ArrayAdapter
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Spinner
+import android.widget.Toast
+import com.convector.david_000.convector_valute.data.ValuteItem
 
-import com.convector.david_000.convector_valute.data.ValuteItem;
-
-import java.util.List;
-
-public class MainActivity extends Activity implements ValuteView, View.OnClickListener{
-    private Spinner spinnerValueFrom,spinnerValueTo;
-    private EditText course, contentValueTo,contentValueFrom;
-    private ArrayAdapter<ValuteItem>adapter;
-
-    ValutePresenter presenter;
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+class MainActivity : Activity(), ValuteView, View.OnClickListener {
+    private var spinnerValueFrom: Spinner? = null
+    private var spinnerValueTo: Spinner? = null
+    private var course: EditText? = null
+    private var contentValueTo: EditText? = null
+    private var contentValueFrom: EditText? = null
+    private var adapter: ArrayAdapter<ValuteItem>? = null
+    var presenter: ValutePresenter? = null
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
 
         // Получаем экземпляр элемента Spinner
-        spinnerValueFrom= (Spinner) findViewById(R.id.spinner_value_from);
-        spinnerValueTo = (Spinner) findViewById(R.id.spinner_value_to);
-        adapter = new ValueItemsAdapter(MainActivity.this, android.R.layout.simple_spinner_item);
-        spinnerValueFrom.setAdapter(adapter);
-        spinnerValueTo.setAdapter(adapter);
-        contentValueFrom=(EditText)findViewById(R.id.content_value_from);
-        course=(EditText)findViewById(R.id.course);
-        course.setEnabled(false);
-        contentValueTo =(EditText)findViewById(R.id.content_value_to);
-        contentValueTo.setEnabled(false);
-        Button reset = (Button)findViewById(R.id.reset);
-        Button calculate=(Button)findViewById(R.id.calculate);
-
-        presenter = new ValutePresenterImpl();
-        presenter.setView(this);
-
-        reset.setOnClickListener(this);
-        calculate.setOnClickListener(this);
+        spinnerValueFrom = findViewById<View>(R.id.spinner_value_from) as Spinner
+        spinnerValueTo = findViewById<View>(R.id.spinner_value_to) as Spinner
+        adapter = ValueItemsAdapter(this@MainActivity, android.R.layout.simple_spinner_item)
+        spinnerValueFrom!!.adapter = adapter
+        spinnerValueTo!!.adapter = adapter
+        contentValueFrom = findViewById<View>(R.id.content_value_from) as EditText
+        course = findViewById<View>(R.id.course) as EditText
+        course!!.isEnabled = false
+        contentValueTo = findViewById<View>(R.id.content_value_to) as EditText
+        contentValueTo!!.isEnabled = false
+        val reset = findViewById<View>(R.id.reset) as Button
+        val calculate = findViewById<View>(R.id.calculate) as Button
+        presenter = ValutePresenterImpl()
+        (presenter as ValutePresenterImpl).setView(this)
+        reset.setOnClickListener(this)
+        calculate.setOnClickListener(this)
     }
 
-    @Override
-    public void onClick(View v) {
-        switch (v.getId()) {
-            case R.id.reset:
-                clickReset();
-                break;
-            case R.id.calculate:
-                clickCalculate();
-                break;
+    override fun onClick(v: View) {
+        when (v.id) {
+            R.id.reset -> clickReset()
+            R.id.calculate -> clickCalculate()
         }
     }
 
-    private void clickReset(){
-        course.setText("");
-        contentValueFrom.setText("");
-        contentValueTo.setText("");
+    private fun clickReset() {
+        course!!.setText("")
+        contentValueFrom!!.setText("")
+        contentValueTo!!.setText("")
     }
 
-    private void clickCalculate(){
-        double countValute= 0;
-
+    private fun clickCalculate() {
+        var countValute = 0.0
         try {
-            countValute  = Double.parseDouble(contentValueFrom.getText().toString().replace(',', '.'));
-        }catch (NumberFormatException e){
-            Toast.makeText(this, R.string.error, Toast.LENGTH_SHORT).show();
+            countValute = contentValueFrom!!.text.toString().replace(',', '.').toDouble()
+        } catch (e: NumberFormatException) {
+            Toast.makeText(this, R.string.error, Toast.LENGTH_SHORT).show()
         }
-
-        ValuteItem valuteItemFrom = adapter.getItem(spinnerValueFrom.getSelectedItemPosition());
-        ValuteItem valuteItemTo = adapter.getItem(spinnerValueTo.getSelectedItemPosition());
-
-        if(valuteItemTo != null) {
-            presenter.ConvertValute(valuteItemFrom, valuteItemTo, countValute);
+        val valuteItemFrom = adapter!!.getItem(spinnerValueFrom!!.selectedItemPosition)
+        val valuteItemTo = adapter!!.getItem(spinnerValueTo!!.selectedItemPosition)
+        if (valuteItemTo != null) {
+            presenter!!.ConvertValute(valuteItemFrom, valuteItemTo, countValute)
         }
     }
 
-    @Override
-    public void setResult(double result) {
-        contentValueTo.setText(String.valueOf(result));
+    override fun setResult(result: Double) {
+        contentValueTo!!.setText(result.toString())
     }
 
-    @Override
-    public void setExchangeRate(Double rate) {
-        course.setText(String.valueOf(rate));
+    override fun setExchangeRate(rate: Double) {
+        course!!.setText(rate.toString())
     }
 
-    @Override
-    public void updateValutes(List<ValuteItem> data) {
-        adapter.clear();
-        if (data != null){
-            for (ValuteItem valuteItem : data) {
-                adapter.insert(valuteItem, adapter.getCount());
+    override fun updateValutes(data: List<ValuteItem>) {
+        adapter!!.clear()
+        if (data != null) {
+            for (valuteItem in data) {
+                adapter!!.insert(valuteItem, adapter!!.count)
             }
         }
-        adapter.notifyDataSetChanged();
+        adapter!!.notifyDataSetChanged()
     }
 }
